474fe3b7023cc87f5e8997ffd84cf1dfc0194879
Naresh <nareshmurugan@swaas.net>
2023-01-19 15:37:57 +0530

    made changes

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 #cd gittest
 git init
-# git checkout -b main
 git clone https://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@github.com/naresh-swaas/flaskapp.git main
+git checkout -b main
 git rm --cached main
 # rm -r *
 echo "made changes t00" > main/text.txt
 # git pull https://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@github.com/naresh-swaas/flaskapp.git main
 git add .
 git commit -m "made changes"
-git push https://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@github.com/naresh-swaas/flaskapp.git
+git push --set-upstream https://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@github.com/naresh-swaas/flaskapp.git
